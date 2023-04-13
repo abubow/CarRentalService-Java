7cf1f6e85a5a98e0f4a6a03cd69e5eba376dc5df
Syed Mohammad Abuzar <40357673+abubow@users.noreply.github.com>
2023-04-13 20:53:31 +0500

    fixed typing error in constructors

--- Main.java
+++ Main.java
@@ -88,38 +88,38 @@ class Main {
 
   // customer menu method
   // public static void customerMenu(Customer customer) {
-  //   // Scanner object
-  //   Scanner scanner = new Scanner(System.in);
-  //   // customer menu
-  //   while (true) {
-  //     System.out.println("Welcome " + customer.getName());
-  //     System.out.println("1. View Cars");
-  //     System.out.println("2. View Bookings");
-  //     System.out.println("3. Book a Car");
-  //     System.out.println("4. Cancel a Booking");
-  //     System.out.println("5. Logout");
-  //     System.out.print("Enter your choice: ");
-  //     int choice = scanner.nextInt();
-  //     scanner.nextLine();
-  //     switch (choice) {
-  //       case 1:
-  //         viewCars();
-  //         break;
-  //       case 2:
-  //         viewBookings(customer);
-  //         break;
-  //       case 3:
-  //         bookCar(customer);
-  //         break;
-  //       case 4:
-  //         cancelBooking(customer);
-  //         break;
-  //       case 5:
-  //         return;
-  //       default:
-  //         System.out.println("Invalid choice");
-  //     }
-  //   }
+  // // Scanner object
+  // Scanner scanner = new Scanner(System.in);
+  // // customer menu
+  // while (true) {
+  // System.out.println("Welcome " + customer.getName());
+  // System.out.println("1. View Cars");
+  // System.out.println("2. View Bookings");
+  // System.out.println("3. Book a Car");
+  // System.out.println("4. Cancel a Booking");
+  // System.out.println("5. Logout");
+  // System.out.print("Enter your choice: ");
+  // int choice = scanner.nextInt();
+  // scanner.nextLine();
+  // switch (choice) {
+  // case 1:
+  // viewCars();
+  // break;
+  // case 2:
+  // viewBookings(customer);
+  // break;
+  // case 3:
+  // bookCar(customer);
+  // break;
+  // case 4:
+  // cancelBooking(customer);
+  // break;
+  // case 5:
+  // return;
+  // default:
+  // System.out.println("Invalid choice");
+  // }
+  // }
   // }
 
   // login admin method
@@ -174,19 +174,25 @@ class Main {
     Scanner scanner = new Scanner(System.in);
     // register customer menu
     while (true) {
+      // public Customer(String name, int age, String gender, String email, String
+      // phone, String password) {
       System.out.println("Enter your name: ");
       String name = scanner.nextLine();
+      System.out.println("Enter your age: ");
+      int age = scanner.nextInt();
+      scanner.nextLine();
+      String gender = "";
+      do {
+        System.out.println("Enter your gender (M/F): ");
+        gender = scanner.nextLine();
+      } while (gender.toLowerCase() != "m" || gender.toLowerCase() != "f");
       System.out.println("Enter your email: ");
       String email = scanner.nextLine();
       System.out.println("Enter your password: ");
       String password = scanner.nextLine();
       System.out.println("Enter your phone number: ");
       String phoneNumber = scanner.nextLine();
-      System.out.println("Enter your address: ");
-      String address = scanner.nextLine();
-      System.out.println("Enter your license number: ");
-      String licenseNumber = scanner.nextLine();
-      Customer customer = new Customer(name, email, password, phoneNumber, address, licenseNumber);
+      Customer customer = new Customer(name, age, gender, email, phoneNumber, password);
       if (carRentalSystem.addCustomer(customer)) {
         System.out.println("Customer registered successfully");
       } else {
@@ -201,17 +207,25 @@ class Main {
     Scanner scanner = new Scanner(System.in);
     // register admin menu
     while (true) {
+      // public Admin(String name, int age, String gender, String email, String phone,
+      // String password) {
       System.out.println("Enter your name: ");
       String name = scanner.nextLine();
+      System.out.println("Enter your age: ");
+      int age = scanner.nextInt();
+      scanner.nextLine();
+      String gender = "";
+      do {
+        System.out.println("Enter your gender (M/F): ");
+        gender = scanner.nextLine();
+      } while (gender.toLowerCase() != "m" || gender.toLowerCase() != "f");
       System.out.println("Enter your email: ");
       String email = scanner.nextLine();
-      System.out.println("Enter your password: ");
-      String password = scanner.nextLine();
       System.out.println("Enter your phone number: ");
       String phoneNumber = scanner.nextLine();
-      System.out.println("Enter your address: ");
-      String address = scanner.nextLine();
-      Admin admin = new Admin(name, email, password, phoneNumber, address);
+      System.out.println("Enter your password: ");
+      String password = scanner.nextLine();
+      Admin admin = new Admin(name, age, gender, email, phoneNumber, password);
       if (carRentalSystem.addAdmin(admin)) {
         System.out.println("Admin registered successfully");
       } else {
